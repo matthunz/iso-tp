288668f0b7f74860fe963e7c0cf047c1d6159e68
Matt Hunzinger <matthunz2@gmail.com>
2023-06-03 21:44:57 -0400

    Create ConsecutiveReader from transaction

--- src/lib.rs
+++ src/lib.rs
@@ -11,14 +11,85 @@ pub mod frame;
 pub use frame::Frame;
 use frame::{FlowKind, Kind};
 
-pub struct Reader<'a, T, R> {
+pub struct ConsecutiveReader<'a, T, R> {
+    tranaction: ConsecutiveTransaction<'a, T, R>,
+    block_len: u8,
+    st: u8,
+}
+
+impl<T, R> AsyncRead for ConsecutiveReader<'_, T, R>
+where
+    T: Sink<Frame> + Unpin,
+    R: Stream<Item = Frame> + Unpin,
+{
+    type Error = ();
+
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context,
+        buf: &mut [u8],
+    ) -> Poll<Result<usize, Self::Error>> {
+        let me = &mut *self;
+        loop {
+            let used = ready!(Pin::new(&mut me.tranaction).poll_read(cx, buf))
+                .ok()
+                .unwrap();
+
+            if used == 0 {
+                if !ready!(Pin::new(&mut me.tranaction).poll_resume(cx, me.block_len, me.st)) {
+                    return Poll::Ready(Ok(0));
+                }
+            } else {
+                break Poll::Ready(Ok(used));
+            }
+        }
+    }
+}
+
+pub struct ConsecutiveTransaction<'a, T, R> {
     remaining_frames: u8,
     remaining_bytes: u16,
     consecutive: Consecutive<'a, T, R>,
     is_first_frame_read: bool,
+    is_flushing: bool,
 }
 
-impl<T, R> Reader<'_, T, R> {
+impl<'a, T, R> ConsecutiveTransaction<'a, T, R> {
+    pub fn poll_resume(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context,
+        block_len: u8,
+        st: u8,
+    ) -> Poll<bool>
+    where
+        T: Sink<Frame> + Unpin,
+    {
+        if self.remaining_bytes == 0 {
+            return Poll::Ready(false);
+        }
+
+        if !self.is_flushing {
+            ready!(self.consecutive.transport.tx.poll_ready_unpin(cx))
+                .ok()
+                .unwrap();
+
+            let frame = Frame::flow(FlowKind::Continue, block_len, st);
+            self.consecutive
+                .transport
+                .tx
+                .start_send_unpin(frame)
+                .ok()
+                .unwrap();
+            self.is_flushing = true;
+        } else {
+            ready!(self.consecutive.transport.tx.poll_flush_unpin(cx))
+                .ok()
+                .unwrap();
+        }
+
+        Poll::Ready(true)
+    }
+
     pub async fn resume(&mut self, block_len: u8, st: u8) -> bool
     where
         T: Sink<Frame> + Unpin,
@@ -52,9 +123,17 @@ impl<T, R> Reader<'_, T, R> {
     {
         self.consecutive.abort().await
     }
+
+    pub fn reader(self, block_len: u8, st: u8) -> ConsecutiveReader<'a, T, R> {
+        ConsecutiveReader {
+            tranaction: self,
+            block_len,
+            st,
+        }
+    }
 }
 
-impl<T, R> AsyncRead for Reader<'_, T, R>
+impl<T, R> AsyncRead for ConsecutiveTransaction<'_, T, R>
 where
     R: Stream<Item = Frame> + Unpin,
 {
@@ -99,18 +178,19 @@ pub struct Consecutive<'a, T, R> {
 }
 
 impl<'a, T, R> Consecutive<'a, T, R> {
-    pub async fn accept(self, block_len: u8, st: u8) -> Reader<'a, T, R>
+    pub async fn accept(self, block_len: u8, st: u8) -> ConsecutiveTransaction<'a, T, R>
     where
         T: Sink<Frame> + Unpin,
     {
         let frame = Frame::flow(FlowKind::Continue, block_len, st);
         self.transport.tx.send(frame).await.ok().unwrap();
 
-        Reader {
+        ConsecutiveTransaction {
             remaining_frames: block_len,
             remaining_bytes: self.first_frame.first_len(),
             consecutive: self,
             is_first_frame_read: false,
+            is_flushing: false,
         }
     }
 
@@ -131,12 +211,43 @@ impl<'a, T, R> Consecutive<'a, T, R> {
     }
 }
 
-pub enum Transaction<'a, T, R> {
+pub struct Reader<'a, T, R> {
+    read: Read<'a, T, R>,
+}
+
+impl<T, R> AsyncRead for Reader<'_, T, R>
+where
+    T: Sink<Frame> + Unpin,
+    R: Stream<Item = Frame> + Unpin,
+{
+    type Error = ();
+
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context,
+        buf: &mut [u8],
+    ) -> Poll<Result<usize, Self::Error>> {
+        match self.read {
+            Read::Single { ref frame } => {
+                let data = frame.single_data();
+                buf[..data.len()].copy_from_slice(data);
+
+                Poll::Ready(Ok(data.len()))
+            }
+            Read::Consecutive(ref mut consecutive) => {
+             
+                todo!()
+            }
+        }
+    }
+}
+
+pub enum Read<'a, T, R> {
     Single { frame: Frame },
     Consecutive(Consecutive<'a, T, R>),
 }
 
-impl<'a, T, R> Transaction<'a, T, R> {
+impl<'a, T, R> Read<'a, T, R> {
     pub fn single(self) -> Option<Frame> {
         if let Self::Single { frame } = self {
             Some(frame)
@@ -190,14 +301,14 @@ impl<T, R> Transport<T, R> {
         Self { tx, rx }
     }
 
-    pub async fn transaction(&mut self) -> Transaction<T, R>
+    pub async fn read(&mut self) -> Read<T, R>
     where
         R: Stream<Item = Frame> + Unpin,
     {
         let frame = self.rx.next().await.unwrap();
         match frame.kind().unwrap() {
-            Kind::Single => Transaction::Single { frame },
-            Kind::First => Transaction::Consecutive(Consecutive {
+            Kind::Single => Read::Single { frame },
+            Kind::First => Read::Consecutive(Consecutive {
                 first_frame: frame,
                 transport: self,
             }),
@@ -217,7 +328,7 @@ mod tests {
         let rx = stream::iter(vec![Frame::single(b"hello").unwrap()]);
 
         let mut tp = Transport::new(tx, rx);
-        let transaction = tp.transaction().await;
+        let transaction = tp.read().await;
         assert_eq!(transaction.single(), Some(Frame::single(b"hello").unwrap()));
     }
 
@@ -232,7 +343,7 @@ mod tests {
         let rx = stream::iter(vec![first, cons]);
 
         let mut tp = Transport::new(tx, rx);
-        let transaction = tp.transaction().await.consecutive().unwrap();
+        let transaction = tp.read().await.consecutive().unwrap();
 
         let mut reader = transaction.accept(10, 0).await;
         let mut buf = [0; 12];
@@ -253,7 +364,7 @@ mod tests {
         let rx = stream::iter(vec![first, cons]);
 
         let mut tp = Transport::new(tx, rx);
-        let transaction = tp.transaction().await;
+        let transaction = tp.read().await;
 
         let mut buf = [0; 12];
         transaction.read(&mut buf, 10, 0).await;
